59343798a01e21ed33aaf7382c7d55c87768b141
Ely Alamillo <ely0013@gmail.com>
2019-07-12 18:56:51 -0500

    implement clearCache for pull to refresh functionality

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -24,9 +24,14 @@ class StoriesBloc {
     _topIds.sink.add(ids);
   }
 
+  clearCache() {
+    return _repository.clearCache();
+  }
+
   _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel>> cache, int id, _) {
+      (Map<int, Future<ItemModel>> cache, int id, idx) {
+        print(idx);
         cache[id] = _repository.fetchItem(id);
         return cache;
       },
--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -40,6 +40,13 @@ class Repository {
     }
     return item;
   }
+
+  clearCache() async {
+    // await automaticall return a future
+    for (var cache in caches) {
+      await cache.clear();
+    }
+  }
 }
 
 abstract class Source {
@@ -49,4 +56,5 @@ abstract class Source {
 
 abstract class Cache {
   Future<int> addItem(ItemModel item);
+  Future<int> clear();
 }
