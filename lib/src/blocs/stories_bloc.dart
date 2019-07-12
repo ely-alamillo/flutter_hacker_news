eb0224bf85ed30b62cadcb89ff184a9c4febe357
Ely Alamillo <ely0013@gmail.com>
2019-07-12 16:54:29 -0500

    add initial stories bloc and provider

---
+++ lib/src/blocs/stories_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:hacker_news/src/models/item_model.dart';
+import 'package:hacker_news/src/resources/repository.dart';
+
+class StoriesBloc {
+  final _repository = Repository();
+  final _topIds = PublishSubject<List<int>>();
+
+  // getters to streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topIds.close();
+  }
+}
