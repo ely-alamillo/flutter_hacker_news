3e726337eff5c5a4665339a01dba786a0340f452
Ely Alamillo <ely0013@gmail.com>
2019-07-16 20:03:07 -0500

    add route parsing for news_detail page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hacker_news/src/blocs/stories_provider.dart';
+import 'package:hacker_news/src/screens/news_detail.dart';
 import 'package:hacker_news/src/screens/news_list.dart';
 
 class App extends StatelessWidget {
@@ -9,14 +10,24 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'News',
         // home: NewsList(),
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(builder: (context) {
+        final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+        return NewsDetail(itemId: itemId);
+      });
+    }
   }
 }
